37eff73ac785d975ae27303910c86abc56266bbf
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2024-09-02 14:03:31 +0530

    API Endpoints are added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import mongoDBConn from "./db/mongoDbConn.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+const PORT = process.env.PORT || 5000; 
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
 
-app.listen(5000,() => console.log("Server running on port 5000"));
+app.use(express.json()); //to parse the incoming requests with json payloads from req.body
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/messages",messageRoutes);
+
+app.listen(PORT ,() =>{ 
+    mongoDBConn();
+    console.log(`Server running on port ${PORT}`)});
