ec8814285bc238d1a75cdd4807ad68e68840cd65
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2024-09-18 05:15:51 +0530

    UI Screens Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import Home from "./pages/home/home"
+import Login from "./pages/login/Login"
+import SignUp from "./pages/signup/Sinup"
+
 /* eslint-disable react/no-unknown-property */
 function App() {
   
 
   return (
     <>
- 
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Login />
+        <SignUp />
+      </div>
+      <div>
+        <Home />
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,32 @@
+const Message = () => {
+  return (
+    <>
+<div className="py-10">
+  <div className="chat chat-end flex flex">
+    <div className="chat-image avatar">
+        <div className="w-10 h-10 rounded-full">
+        <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+    </div>
+  <div className="flex">
+  <div className="chat-header">
+    Anakin
+    <time className="text-xs opacity-50 mx-10">12:46</time>
+  </div>
+  </div>
+  </div>
+  <div className="flex flex-col justify-center ">
+  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-footer opacity-50">Seen at 12:46</div>
+  </div>
+  </div>
+
+
+
+</>
+  )
+}
+
+export default Message
