2f8d5c98aad2da88ef9cf9da5b992220b81a25a2
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2024-09-27 14:48:28 +0530

    Login screen connected to backend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
+
+	const [inputs,setInputs] = useState({
+		userName:"",
+		password:"",
+	});
+
+	const { loading, login } = useLogin();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(inputs);
+	};
+
+
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
   			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,12 +26,14 @@ const Login = () => {
    					<span className='text-blue-500'> ChatApp</span>
   				</h1>
     
-    				<form>
+    				<form onSubmit={handleSubmit}>
     				<div>
    						<label className='label p-2'>
      							<span className='text-base label-text'>Username</span>
    						</label>
-   					<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+   					<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10'
+						value={inputs.userName}
+						onChange={(e)=>setInputs({...inputs,userName:e.target.value})} />
    					</div>
     
   				<div>
@@ -24,8 +42,10 @@ const Login = () => {
 						</label>
     						<input
    							type='password'
-    							placeholder='Enter Password'
+    						placeholder='Enter Password'
   							className='w-full input input-bordered h-10'
+							value={inputs.password}
+							onChange={(e)=>setInputs({...inputs,password:e.target.value})}
     						/>
   					</div>
     					<Link to={'/signup'} className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
@@ -33,7 +53,9 @@ const Login = () => {
   						</Link>
     
    					<div>
-   						<button className='btn btn-block btn-sm mt-2'>Login</button>
+					   <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Login"}
+						</button>
    					</div>
     				</form>
    			</div>
