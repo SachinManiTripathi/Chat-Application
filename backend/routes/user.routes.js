3946742350e6eb9003fe56b9c63ad77abea716a9
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2024-09-29 04:44:23 +0530

    Sidebar users list connected to backend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/",protectRoute, getUsers);
+router.get("/",protectRoute, getUsersForSidebar);
 
 export default router;
