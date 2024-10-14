f29c869165fcc598da0b9c86873b0d76b3f01a8d
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2024-10-14 13:52:08 +0530

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,6 +10,7 @@ import { app,server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000; 
 dotenv.config();
 
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse the incoming requests with json payloads from req.body
 app.use(cookieParser());
@@ -16,6 +18,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/messages",messageRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT ,() =>{ 
     mongoDBConn();
