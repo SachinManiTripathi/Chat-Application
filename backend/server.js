65c38c722358d5dbbce371e9fcd32a2e464f16b2
SachinManiTripathi <156107556+SachinManiTripathi@users.noreply.github.com>
2025-01-28 23:14:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,6 @@ app.use(express.json()); //to parse the incoming requests with json payloads fro
 app.use(cors());
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
-
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/messages",messageRoutes);
