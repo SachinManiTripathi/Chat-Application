520dcb5778db057db7e720459baa2406f9930931
SachinManiTripathi <156107556+SachinManiTripathi@users.noreply.github.com>
2025-01-28 21:45:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
