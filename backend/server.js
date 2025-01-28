2440e1d81b3d8b1449a3c6aee233fe676ec43acc
Sachin Mani Tripathi <sachintripathi1605@gmail.com>
2025-01-28 23:03:16 +0530

    Default backend response

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use(express.json()); //to parse the incoming requests with json payloads fro
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/messages",messageRoutes);
